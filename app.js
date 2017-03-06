8b3b52b223b3282a36cb637e90f48a02a1ff00d3
Ankita Ravalji <ankitaravalji@gmail.com>
2017-03-06 15:08:24 -0800

    http sever created and listening on port 8080

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var http = require('http');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -42,5 +43,13 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+//http server
+var server = http.createServer(function(req, res) {
+  res.writeHead(200);
+  res.end('The http server is all set');
+});
+server.listen(8080);
+
 
 module.exports = app;
